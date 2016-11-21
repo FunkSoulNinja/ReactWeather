d46df6655affd1ae32c2713a0f878b4c942b483a
Anthony <theanthonyfalcon@gmail.com>
2016-11-21 14:21:08 -0800

    added examples

--- app/components/Examples.js
+++ app/components/Examples.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Examples = () => {
     return (
-        <h3>Examples</h3>
+        <div>
+            <h1 className="text-centered">Examples</h1>
+            <p>Here are a few examples locations to try out</p>
+            <ol>
+                <li><Link to="/?location=las%20vegas">Las Vegas</Link></li>
+                <li><Link to="/?location=Tokyo">Tokyo</Link></li>
+            </ol>
+        </div>
     );
 };
 
